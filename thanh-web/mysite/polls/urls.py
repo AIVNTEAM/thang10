1da9e79322f9dbcc0e5861fa479a125200473ac2
Bui Cong Thanh <congthanhbkit03@gmail.com>
2018-10-24 21:32:37 +0700

    Django continue...

--- thanh-web/mysite/polls/urls.py
+++ thanh-web/mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views 
 
 urlpatterns = [ 
+	#ex /polls/
 	url(r'^$', views.index, name='index'),
+	#ex /polls/34/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	#ex /polls/34/results/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	#ex /polls/34/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.votes, name='votes'),
 ]
--- thanh-web/mysite/polls/views.py
+++ thanh-web/mysite/polls/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
 # Create your views here.
 def index(request):
-	response = HttpResponse()
-	response.write("<h1>Welcome</h1>")
-	response.write("This is the polls app")
-	return response
+	# response = HttpResponse()
+	# response.write("<h1>Welcome</h1>")
+	# response.write("This is the polls app")
+	# return response
+	lasted_question_list = Question.objects.order_by('-pub_date')[:5]
+	# output = ','.join([q.question_text for q in lasted_question_list])
+	context = {
+		'lasted_question_list': lasted_question_list
+	}
+	return render(request, 'temp1/index.html', context)
+def detail(request, question_id):
+	# return HttpResponse("Detail for question %s" %question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not existe")
+	return render(request, 'temp1/details.html', {'question':question})
+def results(request, question_id):
+	return HttpResponse("results of question %s" %question_id)
+def votes(request, question_id):
+	return HttpResponse("votes on question %s" % question_id)
